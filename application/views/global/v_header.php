903790cea7cede76838308a36f35030e441b719a
toniasep <31252588+toniasep@users.noreply.github.com>
2020-01-19 20:08:03 +0700

    terakhir responsi

--- application/views/global/v_header.php
+++ application/views/global/v_header.php
@@ -7,31 +7,31 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="<?=base_url(); ?>assets/plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/summernote/summernote-bs4.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.css"/>
    
-  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-  <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.js"></script>
+  <script src="<?=base_url().'assets/'?>vendor/jquery/jquery-3.3.1.min.js"></script>
+  <script src="<?=base_url().'assets/'?>vendor/jquery/jquery-ui.js"></script>
+  <link href="<?=base_url().'assets/'?>vendor/jquery/jquery-ui.css" rel="stylesheet">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -55,7 +55,7 @@
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-              <img src="<?=base_url(); ?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+              <img src="<?=base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
               <a href="#" class="d-block"><?=$this->session->userdata('nama')?></a>
@@ -73,7 +73,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?=base_url()?>" class="brand-link">
-      <img src="<?=base_url(); ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="<?=base_url()?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Nama APP</span>
     </a>
@@ -104,13 +104,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="<?=base_url().'transaksi'; ?>" class="nav-link">
+                <a href="<?=base_url().'transaksi'?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Daftar Transaksi</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="<?=base_url().'transaksi/add_transaksi'; ?>" class="nav-link">
+                <a href="<?=base_url().'transaksi/add_transaksi'?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Tambah Transaksi</p>
                 </a>
--- application/views/produk/v_produk.php
+++ application/views/produk/v_produk.php
@@ -79,6 +79,7 @@
 </div>
 
 
+
 <!-- Page level plugins -->
 <script src="<?=base_url().'assets/'?>vendor/datatables/jquery.dataTables.min.js"></script>
 <script src="<?=base_url().'assets/'?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
@@ -87,6 +88,9 @@
 <script src="<?=base_url().'assets/'?>vendor/datatables/pdfmake.min.js"></script>
 <script src="<?=base_url().'assets/'?>vendor/datatables/vfs_fonts.js"></script>
 <script src="<?=base_url().'assets/'?>vendor/datatables/buttons.html5.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="<?=base_url().'assets/'?>js/demo/datatables-demo.js"></script>
 
 <script>
     $(document).ready(function() {
