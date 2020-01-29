438e6b8f37eb91b6ebe3ca80e21f488f0d9f0099
Rizalu Ilman <ilmanrizalu@gmail.com>
2020-01-30 01:28:13 +0700

    asasdasd

--- application/views/global/v_upgrade.php
+++ application/views/global/v_upgrade.php
@@ -15,8 +15,49 @@
 
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-        
+      <div class="container pb-5">
+        <div class="row justify-content-center">
+          <div class="col-lg-5 px-3 py-5" style="background-color: #ffffff;">
+            <h4 align="center">Terima Kasih Sudah Melakukan Upgrade Paket</h4>
+            <hr style="">
+            <center>
+            <table border="0">
+              <tr>
+                <td style="width: 200px; text-align: left;">Kode Transaksi</td>
+                <td style="width: 200px; text-align: right;">Status</td>
+              </tr>
+              <tr>
+                <td style="width: 200px; text-align: left;">123456789</td>
+                <td style="width: 200px; text-align: right;">BELUM BAYAR</td>
+              </tr>
+            </table></center><br><hr>
+            <p style="text-align: center;">Silahkan Transfer Dana Sebesar:</p>
+            <p class="h5" style="text-align: center;">Rp 500.000</p>
+            <p style="text-align: center;">ke salah satu bank dibawah ini:</p>
+            <hr>
+            <center>
+            <table border="0">
+              <tr>
+                <td style="width: 80px; text-align: left;"><p class="h5">BCA</p></td>
+                <td style="width: 100px; text-align: left;"><p>123456789</p></td>
+              </tr>
+              <tr>
+                <td style="width: 80px; text-align: left;"><p class="h5">BNI</p></td>
+                <td style="width: 100px; text-align: left;"><p>123456789</p></td>
+              </tr>
+              <tr>
+                <td style="width: 80px; text-align: left;"><p class="h5">BRI</p></td>
+                <td style="width: 100px; text-align: left;"><p>123456789</p></td>
+              </tr>
+              <tr>
+                <td colspan="2" style="text-align: center;"><p>A/N KLOLA</p></td>
+              </tr>
+            </table>
+            <hr>
+            <input type="submit" class="btn btn-primary" value="KONFIRMASI PEMBAYARAN" style="background-color: #0f4c75; border-color: #0f4c75;">
+            </center>
+          </div>
+        </div>
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
